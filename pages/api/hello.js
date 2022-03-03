ada325966c48fcd7d9918982b216f3c018e6b82a
diikshaaaa <diksha.grover@crownstack.com>
2022-03-03 12:27:07 +0530

    leanr the use of api routes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// API is the acronym for Application Programming Interface
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // req is an instance of http.IncomingMessage, plus some pre-built middlewares.
+  // res is an instance of http.ServerResponse, plus some helper functions.
+  res.status(200).json({ name: 'Diksha' })
+  // res.status(code) - A function to set the status code. code must be a valid HTTP status code
+  // HTTP 200 OK success status response code indicates that the request has succeeded and  and returns a message property with the value as a JSON response.
 }
+// a good use case for an API Route is:
+// Saving incoming data to your database
+// Securely communicating with a third-party API
+// Previewing draft content from your CMS
